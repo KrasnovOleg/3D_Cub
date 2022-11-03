9a0868a2d469ccb24dd1c309c845531816cd7e09
KrasnovOleg <krasnovoleg232@gmail.com>
2022-11-04 02:16:01 +0700

    3D_Cub
    Загружен  3D_Cub

---
+++ Spawn_3D_Cube/Assets/Scripts/Cub_movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cub_movement : MonoBehaviour
+{
+     float speedCub ;
+    float Drop;
+     public Text Speed;
+    public Text DropText;
+
+    public GameObject Cub;
+
+
+    
+    void Update()
+    {
+        Speed = GameObject.Find("SpeedText").GetComponent<Text>();
+        DropText = GameObject.Find("DropText").GetComponent<Text>();
+
+        speedCub = float.Parse( Speed.text);
+        Drop = float.Parse(DropText.text);
+
+
+
+        if (transform.position.z > 44|| transform.position.z >Drop)// Удаление объекта
+            Destroy(Cub);
+        Cub.transform.Translate(0, 0, speedCub * Time.deltaTime);
+
+    }
+}
---
+++ Spawn_3D_Cube/Assets/Scripts/Spawn_Cub.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class Spawn_Cub : MonoBehaviour
+{
+    public GameObject Cub;
+  //  [SerializeField] private float SpeedSpawn = 6;
+    [SerializeField] private Text  SpeedSpawntext;
+     float SpeedSpawncount ;
+
+
+
+    void Start()//спавн предметов
+    {
+
+
+        // SpeedSpawncount = float.Parse(SpeedSpawntext.text);
+
+        // StartCoroutine(SpawnCD());
+        StartCoroutine(SpawnCD());
+    }
+
+
+
+
+    void Repeat()
+    {
+
+        if (float.TryParse(SpeedSpawntext.text, out SpeedSpawncount))
+        {
+            SpeedSpawncount = float.Parse(SpeedSpawntext.text);
+           
+        }
+ StartCoroutine(SpawnCD());
+
+    }
+    IEnumerator SpawnCD()
+    {
+        
+
+        if (float.TryParse(SpeedSpawntext.text, out SpeedSpawncount))
+        {
+            Instantiate(Cub, new Vector3(0f, 3f, -11f), Quaternion.identity);
+        }            yield return new WaitForSeconds(SpeedSpawncount);// Время задержки спавна
+
+        Repeat();
+
+
+
+
+
+
+
+
+    }
+
+
+
+    private void Update()
+    {
+
+        
+           
+        
+
+
+    }
+
+
+
+
+
+
+
+}
